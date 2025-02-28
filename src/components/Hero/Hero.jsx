3a559bbb893b4b288ad6a08088fb898262c87b09
Isaac Porter <112592768+isaacporter1@users.noreply.github.com>
2025-02-28 15:54:30 -0700

    File Upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Isaac Porter</h1>
+        <p className={styles.description}>
+          I'm a student at Weber State University studying Computer Science. I have 3 years of experience using Python and
+          Pandas, automating tasks for a private school in Utah.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/StudMuffin.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
