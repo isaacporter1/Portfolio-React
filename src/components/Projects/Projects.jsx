9d0b9b6dcd9e59b6189507fe26721751001f6f7a
Isaac Porter <112592768+isaacporter1@users.noreply.github.com>
2025-05-11 13:28:56 -0600

    Fixing Swiper for projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -8,54 +9,45 @@ import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-import styles from "./Projects.module.css";
-
 import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import styles from "./Projects.module.css";
 
 export const Projects = () => {
-
-  // const swiper = new Swiper(...);
-  // return (
-  //   <section className={styles.container} id="projects">
-  //     <h2 className={styles.title}>Projects</h2>
-  //     <div className={styles.projects}>
-  //       {projects.map((project, id) => {
-  //         return <ProjectCard key={id} project={project} />;
-  //       })}
-  //     </div>
-  //   </section>
-  // );
-
   return (
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
-      <>
-        <Swiper
-          className={styles.mySwiper}
-          effect={'coverflow'}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={3}
-          loop={true}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true, //work
-          }}
-          pagination={true}
-          navigation={true}
-          modules={[EffectCoverflow, Pagination, Navigation]}
-        >
-          {projects.map((project, id) => {
-            return <SwiperSlide key={id}> <ProjectCard project={project} /> </SwiperSlide>;
-          })}
-        </Swiper>
-      </>
+      <Swiper
+        className={`${styles.mySwiper} mySwiper`}
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={3}
+        loop={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true, //work
+        }}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={true}
+        navigation={true}
+        modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
+      >
+        {projects.map((project, id) => {
+          return <SwiperSlide /*className={`${styles.mySwiperSlide} ${activeSlideIndex === id ? styles.activeSlide : ''}`}*/
+            key={id}
+          >
+            <ProjectCard project={project} />
+          </SwiperSlide>;
+        })}
+      </Swiper>
     </section>
   );
 };
