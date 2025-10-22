b8df66414b984883674ee31a5d4ae77e976ba418
isaacporter1 <isaacporter1@mail.weber.edu>
2025-10-22 13:10:03 -0600

    Fixing LinkedIn link

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/isaac-porter-8582a92a6?trk=people-guest_people_search-card" target="_blank" rel="noopener noreferrer">linkedin.com/Isaacporter</a>
+          <a href="https://www.linkedin.com/in/isaac-porter-8582a92a6" target="_blank" rel="noopener noreferrer">linkedin.com/Isaacporter</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
